4e06d824ebcd95b34a4d525b3bf99b2e7d9b66db
Sri Harsha Chilakapati <hello@goharsha.com>
2021-02-13 15:44:32 +0530

    Add a toolbar and configure navigation

--- solution/app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ solution/app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(binding.toolbar)
+        NavigationUI.setupWithNavController(binding.toolbar, findNavController(R.id.nav_host_fragment))
     }
 }
